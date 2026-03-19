5cffbdaa29b69f624a92738c0f8242f9ff0d9c6d
klaegkev <112164250+klaegkev@users.noreply.github.com>
2026-03-19 07:45:06 +0100

    Add files via upload

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,32 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 18
+config.color_scheme = "Catppuccin Mocha"
+config.window_decorations = "TITLE|RESIZE"
+config.enable_tab_bar = false
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+--config.window_background_opacity = 0.8
+
+-- MacOs
+--config.macos_window_background_blur = 30
+
+-- Windows
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- return { "powershell.exe" }
+	config.default_prog =
+		{ "powershell", "-NoExit", "-Command", "starship init powershell | out-string | Invoke-Expression" }
+end
+config.win32_system_backdrop = "Acrylic"
+
+-- Linux https://github.com/wezterm/wezterm/issues/4962
+config.enable_wayland = false
+
+return config
